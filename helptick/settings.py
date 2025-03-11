b87f310ae59ffd223d6b97b998e1ffdbd069ff28
craig-grant630 <pc8hxmen@students.codeinstitute.net>
2025-03-11 12:16:00 +0000

    Set up for early deployment

--- helptick/settings.py
+++ helptick/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-5l#idg-*e0%&=e3^x)lo$9nb73#%q!e&ztj(@^!b!aakr9qpax"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost']
 
 
 # Application definition
