2404e1f779bba75a6ea39637fe3522c9c9f8d5ae
craig-grant630 <pc8hxmen@students.codeinstitute.net>
2025-03-30 18:48:57 +0000

    Create super user and display users model for admin to edit

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        *UserAdmin.fieldsets, 
+        (
+            'Customer or Enginner', 
+            {
+                'fields': (
+                    'is_customer', 
+                    'is_engineer'
+                )
+            }
+        )
+    )
+
+admin.site.register(User, CustomUserAdmin)
