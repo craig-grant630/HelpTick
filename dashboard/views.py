16a0a6bb2aa968f023dcc4f5bb7ab72f4b51d73b
craig-grant630 <pc8hxmen@students.codeinstitute.net>
2025-04-02 19:46:03 +0000

    Add view for engineer dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,5 +6,7 @@ def dashboard(request):
     user = request.user  # Get the currently logged-in user
     if user.is_customer:  # Check if the user is a customer
         return render(request, 'dashboard/customer_dashboard.html')
+    elif request.user.is_engineer:
+        return render(request, 'dashboard/engineer_dashboard.html')
     elif user.is_superuser:
         return render(request, 'dashboard/admin_dashboard.html')
