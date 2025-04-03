0df53575bb0d88e3eb2a82f66c99bdf197e9ca79
craig-grant630 <pc8hxmen@students.codeinstitute.net>
2025-04-03 22:06:46 +0000

    Add customer active tickets html and bootstrap styling

--- helptick/settings.py
+++ helptick/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost']
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django.contrib.humanize',
     "widget_tweaks",
     "accounts",
     "dashboard",
