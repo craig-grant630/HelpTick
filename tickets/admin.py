ee1b2eda4d7b1ec9375ecf1912e72d3f28bbca21
craig-grant630 <pc8hxmen@students.codeinstitute.net>
2025-04-03 22:50:36 +0000

    Add tickets model to the admin to modify, create and delete tickets

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Ticket
+
+# Register your Ticket model here
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('ticket_id', 'ticket_title', 'customer', 'created_on', 'status')  # What fields to display in the list view
+    search_fields = ['ticket_id', 'ticket_title', 'customer__email']  # Make some fields searchable
+    list_filter = ['status', 'customer']  # Add filters for easier admin filtering
+
+admin.site.register(Ticket, TicketAdmin)
