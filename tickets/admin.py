66d7da6e2eebbb4ac7161cf611ab17e2066bf2a3
craig-grant630 <pc8hxmen@students.codeinstitute.net>
2025-04-04 20:55:38 +0000

    Add custom border styles for dashboards and create ticket

--- tickets/admin.py
+++ tickets/admin.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from .models import Ticket
 
-# Register your Ticket model here
+
 class TicketAdmin(admin.ModelAdmin):
     list_display = ('ticket_id', 'ticket_title', 'customer', 'created_on', 'status')  # What fields to display in the list view
     search_fields = ['ticket_id', 'ticket_title', 'customer__email']  # Make some fields searchable
