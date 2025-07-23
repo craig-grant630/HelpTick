13f1d5e4a9489bfc3bae9bfae85de09f26401672
craig-grant630 <pc8hxmen@students.codeinstitute.net>
2025-07-23 13:10:48 +0000

    Update ticket views for validator

--- tickets/views.py
+++ tickets/views.py
@@ -45,6 +45,7 @@ def create_ticket(request):
         context = {'form': form}
         return render(request, 'tickets/create_ticket.html', context)
 
+
 @login_required
 def customer_active_tickets(request):
     ticket_list = Ticket.objects.filter(
@@ -63,6 +64,7 @@ def customer_active_tickets(request):
     }
     return render(request, 'tickets/customer_active_tickets.html', context)
 
+
 @login_required
 def ticket_details(request, ticket_id):
     ticket = Ticket.objects.get(ticket_id=ticket_id)
@@ -74,6 +76,7 @@ def ticket_details(request, ticket_id):
         messages.error(request, "You are not allowed to edit this ticket.")
         return redirect('dashboard')
     return render(request, 'tickets/ticket_details.html', {'ticket': ticket})
+
 
 @login_required
 def ticket_queue(request):
@@ -93,6 +96,7 @@ def ticket_queue(request):
         'tickets/ticket_queue.html',
         {'tickets': tickets, 'search_query': search_query}
     )
+
 
 @login_required
 def assign_ticket(request, ticket_id):
@@ -129,6 +133,7 @@ def assign_ticket(request, ticket_id):
             {'form': form, 'ticket': ticket}
         )
 
+
 @login_required
 def engineer_active_tickets(request):
     if not request.user.is_engineer and not request.user.is_superuser:
@@ -150,12 +155,13 @@ def engineer_active_tickets(request):
     }
     return render(request, 'tickets/engineer_active_tickets.html', context)
 
+
 @login_required
 def resolve_ticket(request, ticket_id):
     ticket = Ticket.objects.get(ticket_id=ticket_id)
     if (
-    request.user != ticket.engineer and
-    not request.user.is_superuser
+        request.user != ticket.engineer and
+        not request.user.is_superuser
     ):
         messages.error(request, "You are not allowed to edit this ticket.")
         return redirect('dashboard')
@@ -167,6 +173,7 @@ def resolve_ticket(request, ticket_id):
         ticket.save()
         messages.success(request, 'Ticket is now resolved and closed')
         return redirect('dashboard')
+
 
 @login_required
 def customer_resolved_tickets(request):
@@ -185,6 +192,7 @@ def customer_resolved_tickets(request):
         'page_obj': page_obj,
     }
     return render(request, 'tickets/customer_resolved_tickets.html', context)
+
 
 @login_required
 def engineer_resolved_tickets(request):
@@ -207,6 +215,7 @@ def engineer_resolved_tickets(request):
     }
     return render(request, 'tickets/engineer_resolved_tickets.html', context)
 
+
 @login_required
 def delete_ticket(request, ticket_id):
     ticket = Ticket.objects.get(ticket_id=ticket_id)
@@ -222,6 +231,7 @@ def delete_ticket(request, ticket_id):
         'tickets/delete_ticket_confirm.html',
         {'tickets': ticket}
     )
+
 
 @login_required
 def edit_ticket_description(request, ticket_id):
